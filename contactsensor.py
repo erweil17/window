1a657182aa8616ea417fdde9fb3af86e3ea095ea
erweil17 <eric.weiler.10@gmail.com>
2018-11-06 21:58:52 -0800

    Update contactsensor.py

--- contactsensor.py
+++ contactsensor.py
@@ -1,3 +1,5 @@
+#This program is for testing the window contact sensor.
+#The red LED should light up whenever the sensor is making contact (or the window is closed)
 from gpiozero import LED, Button
 import time
 
